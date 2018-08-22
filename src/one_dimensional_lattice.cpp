ae079440a40772a03ac89b5926b10759f3b837db
Will Vining <wfvining@gmail.com>
2018-08-21 20:10:12 -0600

    Make one_d_lattice multi-threaded

--- src/one_dimensional_lattice.cpp
+++ src/one_dimensional_lattice.cpp
@@ -1,8 +1,15 @@
 #include "OneDLattice.hpp"
 
 #include <iostream>
+#include <future>
+#include <map>
+#include <utility>
 
 #define NUM_REPLICAS 100
+
+int num_agents;
+double density_step;
+int seed;
 
 bool evaluate_ca(OneDLattice& lattice)
 {
@@ -26,32 +33,46 @@ bool evaluate_ca(OneDLattice& lattice)
    }
 }
 
+std::map<double,double> evaluate_radius(int radius)
+{
+   OneDLattice lattice(num_agents, radius);
+   lattice.Seed(seed);
+
+   std::map<double, double> results;
+   for(double density = 0.0; density <= 1.001; density += density_step)
+   {
+      int correct = 0;
+      for(int i = 0; i < NUM_REPLICAS; i++)
+      {
+         lattice.SetDensity(density);
+         if(evaluate_ca(lattice))
+         {
+            correct++;
+         }
+      }
+      results.emplace(std::make_pair(density, (double)correct / (double)NUM_REPLICAS));
+   }
+   return results;
+}
+
 int main(int argc, char** argv)
 {
    if(argc != 4) return -1;
-   int num_agents = atoi(argv[1]);
-   double density_step = atof(argv[2]);
-   int seed = atoi(argv[3]);
+   num_agents = atoi(argv[1]);
+   density_step = atof(argv[2]);
+   seed = atoi(argv[3]);
 
+   std::map<int,std::future<std::map<double,double>>> futures;
    for(int radius = 1; radius <= num_agents/2; radius++)
    {
-      OneDLattice lattice(num_agents, radius);
-      lattice.Seed(seed);
+      futures.emplace(std::make_pair(radius,std::async(std::launch::any, std::bind(evaluate_radius, radius))));
+   }
 
-      for(double density = 0.0; density <= 1.01; density += density_step)
+   for(auto& future : futures)
+   {
+      for(auto& result : future.second.get())
       {
-         int correct = 0;
-         for(int i = 0; i < NUM_REPLICAS; i++)
-         {
-            lattice.SetDensity(density);
-            if(evaluate_ca(lattice))
-            {
-               correct++;
-            }
-         }
-         std::cout << radius << " "
-                   << density << " "
-                   << (double)correct / (double)NUM_REPLICAS << std::endl;
+         std::cout << future.first << " " << result.first << " " << result.second << std::endl;
       }
    }
    return 0;
