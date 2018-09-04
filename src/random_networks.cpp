00166e066ff60bda650c2332270d1e10aec1f1bc
Will Vining <wfvining@gmail.com>
2018-09-04 12:54:57 -0600

    Make each random network have a particular number of edges.

--- src/random_networks.cpp
+++ src/random_networks.cpp
@@ -8,17 +8,18 @@
 int main(int argc, char**argv)
 {
    int seed = atoi(argv[1]);
-   int k    = atoi(argv[2]);
+   int edges_per_snapshot = atoi(argv[2]);
    std::mt19937_64 gen(seed);
    std::uniform_int_distribution<int> node_dist(0, NUM_NODES-1);
 
    NetworkSnapshot network(NUM_NODES);
-   for(int node = 0; node < NUM_NODES; node++)
+   for(int i = 0; i < 5000; i++)
    {
-      while(network.Degree(node) < k)
+      while(network.EdgeCount() < edges_per_snapshot)
       {
          int u = node_dist(gen);
-         network.AddEdge(node, u);
+         int v = node_dist(gen);
+         network.AddEdge(u, v);
       }
    }
 }
