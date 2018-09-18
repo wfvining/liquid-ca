d6baad7dd6c51b380ac8d95fbc376b4e58aadc3a
Will Vining <wfvining@gmail.com>
2018-09-18 12:21:35 -0600

    Program to sweep the agent density parameter at a given agent speed.

--- src/density_sweep.cpp
+++ src/density_sweep.cpp
@@ -3,8 +3,8 @@
 #include <cstdlib> // atoi
 #include <functional>
 #include <cmath>
-#include <future>
 #include <thread> // hardware_concurrency()
+#include <mutex>
 #include <chrono>
 #include <utility>
 #include <map>
@@ -14,13 +14,28 @@
 
 #include "Model.hpp"
 
-double evaluate_ca(int num_iterations, double speed, double initial_density)
+struct config {
+   int    num_agents;
+   double communication_range;
+   double speed;
+   int    seed;
+   int    num_iterations;
+} model_config;
+
+std::mutex density_lock;
+double next_density = 0.05;
+const double DENSITY_STEP = 0.05;
+const double MAX_DENSITY  = 4.0;
+
+std::mutex output_lock;
+
+double evaluate_ca(int num_iterations, double speed, double initial_density, int arena_size)
 {
    std::uniform_real_distribution<double> heading_distribution(0, 2*M_PI);
    int num_correct = 0;
    for(int iteration = 0; iteration < num_iterations; iteration++)
    {
-      Model m(model_config.arena_size,
+      Model m(arena_size,
               model_config.num_agents,
               model_config.communication_range,
               model_config.seed+iteration,
@@ -28,8 +43,9 @@ double evaluate_ca(int num_iterations, double speed, double initial_density)
               speed);
 
       m.SetMovementRule(RandomWalk());
+      m.RecordNetworkDensityOnly();
 
-      for(int step = 0; step < 5000; step++)
+      for(int step = 0; step < 2500; step++)
       {
          m.Step(majority_rule);
          if(m.CurrentDensity() == 0 || m.CurrentDensity() == 1)
@@ -47,18 +63,62 @@ double evaluate_ca(int num_iterations, double speed, double initial_density)
    return (double) num_correct / num_iterations;
 }
 
+void evaluate_density(double agent_density)
+{
+   double arena_size = sqrt(model_config.num_agents / agent_density);
+   std::map<double, double> results;
+   for(double state_density = 0.0; state_density <= 1.001; state_density += 0.02)
+   {
+      double proportion_correct = evaluate_ca(model_config.num_iterations, model_config.speed,
+                                              state_density, arena_size);
+      results.emplace(state_density, proportion_correct);
+   }
+
+   output_lock.lock(); // lock to avoid interleaving output :/
+   for(auto result : results)
+   {
+      std::cout << agent_density << " "
+                << result.first  << " "
+                << result.second
+                << std::endl;
+   }
+   std::cout << std::endl;
+   output_lock.unlock();
+}
+
+void thread_main()
+{
+   while(true)
+   {
+      density_lock.lock();
+
+      double density = next_density;
+      next_density += DENSITY_STEP;
+
+      density_lock.unlock();
+
+      if(density <= MAX_DENSITY)
+      {
+         evaluate_density(density);
+      }
+      else
+      {
+         return;
+      }
+   }
+}
+
 int main(int argc, char** argv)
 {
    int    opt_char;
    int    sweep_density   = 0;
    double density_step    = 0.01;
    double initial_density = 0.0;
-   int    num_iterations  = 1;
    int    save_state      = 0;
 
    model_config.communication_range = 1;
    model_config.num_agents          = 100;
-   model_config.arena_size          = 1;
+   model_config.num_iterations      = 100;
    model_config.seed                = 1234;
 
    static struct option long_options[] =
@@ -87,7 +147,7 @@ int main(int argc, char** argv)
          break;
 
       case 'i':
-         num_iterations = atoi(optarg);
+         model_config.num_iterations = atoi(optarg);
          break;
 
       case ':':
@@ -106,16 +166,21 @@ int main(int argc, char** argv)
       std::cout << "missing required argument <agent-speed>" << std::endl;
    }
 
-   double speed = atof(argv[optind]);
+   model_config.speed = atof(argv[optind]);
 
-   for(double agent_density = 0.01; agent_density < 2.0; agent_density+=0.01)
+   int num_threads = std::thread::hardware_concurrency();
+
+   std::vector<std::thread> threads;
+
+   for(int i = 0; i < num_threads; i++)
    {
-      double arena_size = sqrt(num_agents / agent_density);
-      for(double state_density = 0.0; state_density <= 1.0; state_density += 0.05)
-      {
-         std::cout << agent_density << " "
-                   << state_density << " "
-                   << evaluate_ca(num_iterations, speed, state_density);
-      }
+      threads.push_back(std::thread(thread_main));
    }
+
+   for(auto& t : threads)
+   {
+      t.join();
+   }
+
+   return 0;
 }
