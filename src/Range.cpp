82adabf869d664780d326f1a44a83c5322341ab5
Will Vining <wfvining@gmail.com>
2019-04-11 13:41:01 -0600

    Added Range class that will be useful for general totalistic rules

---
+++ src/Range.cpp
@@ -0,0 +1,42 @@
+#include "Range.hpp"
+
+Range::Range(double min, double max) :
+   min_(min),
+   max_(max),
+   include_min_(true),
+   include_max_(true)
+{}
+
+Range Range::ExcludeMin() const
+{
+   Range r(*this);
+   r.include_min_ = false;
+   return r;
+}
+
+Range Range::ExcludeMax() const
+{
+   Range r(*this);
+   r.include_max_ = false;
+   return r;
+}
+
+bool Range::Contains(double k) const
+{
+   if(include_max_ && include_min_)
+   {
+      return k <= max_ && k >= min_;
+   }
+   else if(include_max_ && !include_min_)
+   {
+      return k <= max_ && k > min_;
+   }
+   else if(!include_max_ && include_min_)
+   {
+      return k < max_ && k >= min_;
+   }
+   else // if(!include_max_ && !include_min_)
+   {
+      return k < max_ && k > min_;
+   }
+}
